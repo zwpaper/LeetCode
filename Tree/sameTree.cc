73e53119f74266068ccfdaf2fdea1ad6f5695996
zwPapEr <zw.paper@gmail.com>
2015-04-01 20:56:39 +0800

    recorrect the sameTree problem, and improve 1ms

--- Tree/sameTree.cc
+++ Tree/sameTree.cc
@@ -18,29 +18,20 @@ public:
 
     bool frontCampare( TreeNode *node1, TreeNode *node2 )
     {
-    	if( node1 == node2 == NULL )
-    		return true;
-    	else if( node1->val == node2->val )
-    	{
+    	if(( node1 == NULL ) || ( node2 == NULL ))
+    		return (( node1 == NULL ) && ( node2 == NULL ))
+    	if( node1->val == node2->val )
+	    {
     		bool inFlag = true;
-    		if((( node1->left == NULL ) && ( node2->left != NULL )) ||
-    	        (( node1->left != NULL ) && ( node2->left == NULL )))
-    			return false;
-    		else 
-    			inFlag = frontCampare( node1->left, node2->left );
+    		inFlag = frontCampare( node1->left, node2->left );
 
     		if( inFlag == false )
     			return false;
     		else
-    		{
-    			if((( node1->right == NULL ) && ( node2->right != NULL )) ||
-    	        (( node1->right != NULL ) && ( node2->right == NULL )))
-    				return false;
-    			else
-    				return frontCampare( node2->right, node2->right );
-    		}
-    	}
-    	else
-    		return false;
+    			return frontCampare( node1->right, node2->right );
+    		
+	    }
+	    else
+	        return false;
     }
 };
